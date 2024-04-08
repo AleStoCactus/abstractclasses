9c68ffda9296f9cfe9896e3cc183fc44abb605ec
AleStoCactus <alessandropala06@gmail.com>
2024-04-08 10:03:44 +0200

    Classi Astrali

---
+++ src/PanzerSelbstfahrlafetteIfür.java
@@ -0,0 +1,8 @@
+public class PanzerSelbstfahrlafetteIfür  extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Panzer Selbstfahrlafette I für 7,62-cm-PaK 36(r) auf Fahrgestell Panzerkampfwagen II Ausf");
+        PanzerSelbstfahrlafetteIfür panzer = new PanzerSelbstfahrlafetteIfür();
+        panzer.makeNoise();
+    }
+}
